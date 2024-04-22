9f18f9c7a132612b5f1dc2de16a6238c172491f0
Hind Tanouti <tanoutihind@gmail.com>
2024-04-22 10:04:04 +0200

    Première étape

---
+++ Guy.h
@@ -0,0 +1,9 @@
+#pragma once
+int const w = 500;
+int const h = 300;
+
+// Dimensions et position du Guy
+const int wGuy=20;
+const int hGuy=30;
+const int xGuy=25;
+
---
+++ main.cpp
@@ -0,0 +1,16 @@
+// Project Gravity Guy
+// Author: Cyprien MALGOYRE and Hind TANOUTI
+
+#include <Imagine/Graphics.h>
+#include <iostream>
+using namespace Imagine;
+using namespace std;
+#include "Guy.h"
+
+int main(){
+    openWindow(w,h);
+    endGraphics();
+    return 0;
+}
+
+
